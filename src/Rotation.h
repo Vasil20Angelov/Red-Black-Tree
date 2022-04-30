5871f55f591ce493dc1b2c0e225bc9de7d80f0ab
Vasil Angelov <vasil.angelov@lights.digital>
2022-04-30 13:21:48 +0300

    Done Rotation class
    * The Rotation class now supports all 4 types of rotations (RR, LL, RL, LR)
    * Added unit tests for the class methods

--- src/Rotation.h
+++ src/Rotation.h
@@ -1,11 +1,15 @@
 #pragma once
 #include "Node.h"
 
-struct Rotation {
+class Rotation {
+
+public:
 
 	/**
 	* \brief Makes a left rotation
 	* \param The node from where the rotation begins
+	* 
+	* The rotation is performed when there are 2 right-right connections
 	* 
 	* 	a						b
 	*	 \					   / \
@@ -14,20 +18,125 @@ struct Rotation {
 	*	    c
 	* 
 	*/
-	static void left(Node* node)
+	static void left(Node* c)
 	{
-		Node* grandP = node->parent->parent;
-		Node* grandPPar = grandP->parent;
-		if (grandPPar) {
-			if (grandPPar->left == grandP)
-				grandPPar->left = node->parent;
+		Node* a = c->parent->parent;
+		changeParentConnection(a, c);
+
+		// If b has a left child it should be moved as a right child of a
+		a->right = c->parent->left;
+		if (a->right)
+			a->right->parent = a;
+
+		c->parent->left = a;
+	}
+
+	/**
+	* \brief Makes a right rotation
+	* \param The node from where the rotation begins
+	*
+	* The rotation is performed when there are 2 left-left connections
+	* 
+	* 		a						b
+	*	   /					   / \
+	*	  b			=>		      c   a
+	*	 /
+	*   c
+	*
+	*/
+	static void right(Node* c)
+	{
+		Node* a = c->parent->parent;
+		changeParentConnection(a, c);
+
+		// If b has a right child it should be moved as a left child of a
+		a->left = c->parent->right;
+		if (a->left)
+			a->left->parent = a;
+
+		c->parent->right = a;
+	}
+
+
+	/**
+	* \brief Makes a left-right rotation
+	* \param The node from where the rotation begins
+	*
+	* The rotation is performed when there are left-right connections
+	*
+	* 		a							c
+	*	   /(left)					   / \
+	*	  b					=>	      b   a
+	*	   \(right)
+	*       c
+	*
+	*/
+	static void leftRight(Node* c)
+	{
+		Node* b = c->parent;
+		b->parent->left = c;
+		c->parent = b->parent;
+
+		// If c has a left child it should be moved as a right child of b
+		b->right = c->left;
+		if (b->right)
+			b->right->parent = b;
+
+		c->left = b;
+		b->parent = c;
+
+		right(b);
+	}
+
+	/**
+	* \brief Makes a right-left rotation
+	* \param The node from where the rotation begins
+	*
+	* The rotation is performed when there are right-left connections
+	*
+	* 		a                           c
+	*		 \(right)				   / \
+	*		  b				=>		  a   b
+	*		 /(left)		
+	*       c
+	*
+	*/
+	static void rightLeft(Node* c)
+	{
+		Node* b = c->parent;
+		b->parent->right = c;
+		c->parent = b->parent;
+
+		// If c has a right child it should be moved as a left child of b
+		b->left = c->right;
+		if (b->left) 
+			b->left->parent = b;
+
+		c->right = b;
+		b->parent = c;
+
+		left(b);
+	}
+
+private:
+	
+	/**
+	* @brief Changes the parents connections between a,c and the parent of a
+	* 
+	* The child of the parent of a should be changed from a to c and the parent of c should become the parent of a,
+	* while the new parent of a should be c
+	*/
+	static void changeParentConnection(Node* a, Node* c)
+	{
+		Node* aPar = a->parent;
+		if (aPar) {
+			if (aPar->left == a)
+				aPar->left = c->parent;
 			else
-				grandPPar->right = node->parent;
+				aPar->right = c->parent;
 		}
 
-		node->parent->left = grandP;
-		node->parent->parent = grandPPar;
-		grandP->parent = node->parent;
-		grandP->right = nullptr;
+		c->parent->parent = aPar;
+		a->parent = c->parent;
 	}
 };
--- tests/RBT.cpp
+++ tests/RBT.cpp
@@ -53,25 +53,97 @@ TEST_CASE("Allocator")
 TEST_CASE("Rotations")
 {
 	Allocator alc;
-	Node* root = alc.allocate(1);
+	Node* root = alc.allocate(5);
 	
-	SECTION("Left rotation")
+	SECTION("Left")
 	{
 		Node* a = alc.allocate(10, root);
 		Node* b = alc.allocate(20, a);
 		Node* c = alc.allocate(30, b);
+		Node* d = alc.allocate(15, b);
 
 		Rotation::left(c);
 
 		REQUIRE(b->left == a);
 		REQUIRE(b->right == c);
+		REQUIRE(a->right == d);
 		REQUIRE(root->right == b);
 		REQUIRE(b->parent == root);
 		REQUIRE(a->parent == b);
 		REQUIRE(c->parent == b);
-		REQUIRE(a->isLeaf() == true);
+		REQUIRE(d->parent == a);
 		REQUIRE(c->isLeaf() == true);
+		REQUIRE(d->isLeaf() == true);
 
 		alc.deAllocate(b);
+	}
+
+	SECTION("Right")
+	{
+		Node* a = alc.allocate(4, root);
+		Node* b = alc.allocate(2, a);
+		Node* c = alc.allocate(1, b);
+		Node* d = alc.allocate(3, b);
+
+		Rotation::right(c);
+
+		REQUIRE(b->left == c);
+		REQUIRE(b->right == a);
+		REQUIRE(a->left == d);
+		REQUIRE(root->left == b);
+		REQUIRE(b->parent == root);
+		REQUIRE(a->parent == b);
+		REQUIRE(c->parent == b);
+		REQUIRE(d->parent == a);
+		REQUIRE(c->isLeaf() == true);
+		REQUIRE(d->isLeaf() == true);
+
+		alc.deAllocate(b);
+	}
+
+	SECTION("Left-Right")
+	{
+		Node* a = alc.allocate(4, root);
+		Node* b = alc.allocate(1, a);
+		Node* c = alc.allocate(3, b);
+		Node* d = alc.allocate(2, c);
+
+		Rotation::leftRight(c);
+
+		REQUIRE(c->left == b);
+		REQUIRE(c->right == a);
+		REQUIRE(b->right == d);
+		REQUIRE(root->left == c);
+		REQUIRE(c->parent == root);
+		REQUIRE(a->parent == c);
+		REQUIRE(b->parent == c);
+		REQUIRE(d->parent == b);
+		REQUIRE(a->isLeaf() == true);
+		REQUIRE(d->isLeaf() == true);
+
+		alc.deAllocate(c);
+	}
+
+	SECTION("Right-Left")
+	{
+		Node* a = alc.allocate(6, root);
+		Node* b = alc.allocate(9, a);
+		Node* c = alc.allocate(7, b);
+		Node* d = alc.allocate(8, c);
+
+		Rotation::rightLeft(c);
+
+		REQUIRE(c->left == a);
+		REQUIRE(c->right == b);
+		REQUIRE(b->left == d);
+		REQUIRE(root->right == c);
+		REQUIRE(c->parent == root);
+		REQUIRE(a->parent == c);
+		REQUIRE(b->parent == c);
+		REQUIRE(d->parent == b);
+		REQUIRE(a->isLeaf() == true);
+		REQUIRE(d->isLeaf() == true);
+
+		alc.deAllocate(c);
 	}
 }
